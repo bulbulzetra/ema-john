8144c26a32b51fff4881e1e5b77424702143b498
bulbul <bulbulzetra93@gmail.com>
2023-09-12 01:57:15 +0600

    delete btn adde

---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ReviewItem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+const ReviewItem = ({product}) => {
+    const {id, img, price, name, quantity} = product
+    return (
+        <div className='review-item'>
+            <img src={img} alt="" />
+            <div className='review-details'>
+                <p>{name}</p>
+                <p>Price: <span className='text-orange-500'>${price}</span></p>
+                <p>Quantilty: <span className='text-orange-500'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'>
+            <FontAwesomeIcon className='text-red-600 text-2xl' icon={faTrashAlt} />
+
+            </button>
+        </div>
+    );
+};
+
+export default ReviewItem;
