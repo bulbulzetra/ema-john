068fd1a039e6b1cc2457f69dfe0fd997f47d6133
bulbul <bulbulzetra93@gmail.com>
2023-09-13 01:26:26 +0600

    delete btn added

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -3,7 +3,7 @@ import './ReviewItem.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product,handleRemoveFromCart}) => {
     const {id, img, price, name, quantity} = product
     return (
         <div className='review-item'>
@@ -13,7 +13,7 @@ const ReviewItem = ({product}) => {
                 <p>Price: <span className='text-orange-500'>${price}</span></p>
                 <p>Quantilty: <span className='text-orange-500'>{quantity}</span></p>
             </div>
-            <button className='btn-delete'>
+            <button onClick={()=>handleRemoveFromCart(id)} className='btn-delete'>
             <FontAwesomeIcon className='text-red-600 text-2xl' icon={faTrashAlt} />
 
             </button>
